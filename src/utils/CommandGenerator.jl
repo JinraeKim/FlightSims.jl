87569547142ba24e1bb254bcb40386a87825a899
JinraeKim <kjl950403@gmail.com>
2021-06-05 18:51:10 +0900

    Add helix command generator

--- src/utils/CommandGenerator.jl
+++ src/utils/CommandGenerator.jl
@@ -1,8 +1,55 @@
 abstract type AbstractCommandGenerator end
 abstract type AbstractMulticopterCommandGenerator <: AbstractCommandGenerator end
 
+
 function command_generator(cg::AbstractCommandGenerator)
     error("Not implemented")
+end
+
+"""
+# Variables
+pos0 ∈ R^3: initial position
+dir ∈ S^2 (sphere): a unit vector
+dir_perp ∈ S^2 (sphere): a unit vector perp. to `dir`
+r > 0: radius
+ω: angular rate
+θ: phase
+c: coefficient for forward direction
+# Notes
+The basis vectors of the plane perpendicular to the `dir` is determined by a keyword argument
+`dir_ref=[0, 0, 1]`.
+For more details, see the source code.
+"""
+struct HelixCommandGenerator
+    pos0
+    t0
+    dir
+    dir_perp
+    r
+    ω
+    θ
+    c
+end
+function HelixCommandGenerator(pos0, dir=[0, 0, 1],
+        r=1.0, ω=1.0, θ=deg2rad(0),
+        c=1.0;
+        dir_ref=[1, 0, 0], t0=0.0)
+    @assert r > 0
+    @assert norm(cross(dir, dir_ref)) / (norm(dir)*norm(dir_ref)) > 1e-6  # dir and dir_ref should not be parallel
+    dir_unit = dir / norm(dir)  # unit vec
+    dir_perp = cross(dir_unit, cross(dir_ref, dir_unit))
+    dir_perp_unit = dir_perp / norm(dir_perp)
+    HelixCommandGenerator(pos0, t0, dir_unit, dir_perp_unit, r, ω, θ, c)
+end
+
+function command_generator(cg::HelixCommandGenerator)
+    @unpack pos0, t0, dir, dir_perp, r, ω, θ, c = cg
+    dir_perp2 = cross(dir, dir_perp)
+    dir_perp2_unit = dir_perp2 / norm(dir_perp2)
+    return function (t)
+        t̃ = t - t0
+        pos0 + (c*t̃)*dir + r*dir_perp*cos(ω*t + θ) + r*dir_perp2*sin(ω*t + θ)
+    end
 end
 
 """
