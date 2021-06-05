768ff62d2f8c7e10e51552bebbd6784ed137ce4b
JinraeKim <kjl950403@gmail.com>
2021-06-05 20:41:02 +0900

    wip

--- main/backstepping_tracking.jl
+++ main/backstepping_tracking.jl
@@ -25,16 +25,16 @@ function main()
     df = process()(prob, sol; Δt=0.01)
     ts = df.times
     poss = df.states |> Map(state -> state.multicopter.p) |> collect
-    poss_true = ts |> Map(t -> cg(t)) |> collect
+    poss_ref = ts |> Map(t -> cg(t)) |> collect
     ## plot
     # time vs position
     p_pos = plot(; title="position", legend=:outertopright)
     plot!(p_pos, ts, hcat(poss...)'; label=["x" "y" "z"], color="red", ls=[:dash :dot :dashdot])
-    plot!(p_pos, ts, hcat(poss_true...)'; label=["x (true)" "y (true)" "z (true)"], color="black", ls=[:dash :dot :dashdot])
+    plot!(p_pos, ts, hcat(poss_ref...)'; label=["x (ref)" "y (ref)" "z (ref)"], color="black", ls=[:dash :dot :dashdot])
     savefig(p_pos, "t_vs_pos.png")
     # 3d traj
     p_traj = plot3d(; title="position", legend=:outertopright)
     plot!(p_traj, hcat(poss...)'[:, 1], hcat(poss...)'[:, 2], hcat(poss...)'[:, 3]; label="position", color="red")
-    plot!(p_traj, hcat(poss_true...)'[:, 1], hcat(poss_true...)'[:, 2], hcat(poss_true...)'[:, 3]; label="position (true)", color="black")
+    plot!(p_traj, hcat(poss_ref...)'[:, 1], hcat(poss_ref...)'[:, 2], hcat(poss_ref...)'[:, 3]; label="position (ref)", color="black")
     savefig(p_traj, "traj.png")
 end
