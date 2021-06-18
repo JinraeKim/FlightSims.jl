e8ce7042e5568b5732ab653c6a0d76e5e7639ec4
JinraeKim <kjl950403@gmail.com>
2021-06-18 10:47:25 +0900

    Complete a draft form of @log

--- src/APIs/simulation.jl
+++ src/APIs/simulation.jl
@@ -1,53 +1,3 @@
-## Simulation
-# """
-# # Notes
-# - `datum_format` should be a function
-#     - whose input is `(x, t, integrator::DiffEqBase.DEIntegrator)`
-#     and whose output is `nt::NamedTuple`.
-#     - See `src/APIs/data_processing.jl`.
-# - Default settings for saving
-#     - save values "after all other callbacks"
-#     - `saveat=[]` will save data at every time step.
-# """
-# function sim(state0, dyn, p=nothing;
-#         t0=0.0, tf=1.0, solver=nothing,  # DifferentialEquations.jl will find a default solver
-#         callback::DiffEqBase.DECallback=CallbackSet(),
-#         datum_format=nothing,
-#         saveat=nothing,
-#         savestep=nothing,
-#         kwargs...,
-#     )
-#     if saveat == nothing && savestep == nothing
-#         saveat = []  # default
-#     elseif saveat != nothing && savestep == nothing
-#         # nothing
-#     elseif saveat == nothing && savestep != nothing
-#         saveat = t0:savestep:tf
-#     elseif saveat != nothing && savestep != nothing
-#         error("Assign values of either `saveat` or `savestep`")
-#     end
-#     tspan = (t0, tf)
-#     prob = ODEProblem(dyn, state0, tspan, p)
-#     saved_values = nothing
-#     if datum_format != nothing
-#         saved_values = SavedValues(Float64, NamedTuple)
-#         cb_save = SavingCallback(datum_format, saved_values;
-#                                  saveat=saveat, tdir=Int(sign(tspan[2]-tspan[1])))
-#         callback = CallbackSet(callback, cb_save)  # save values "after all other callbacks"
-#     end
-#     sol = solve(prob, solver; callback=callback, kwargs...)
-#     if datum_format == nothing
-#         return prob, sol
-#     else
-#         df = DataFrame(time=saved_values.t)
-#         all_keys = (saved_values.saveval |> Map(keys) |> union)[1]
-#         for key in all_keys
-#             _getproperty(x) = isdefined(x, key) ? getproperty(x, key) : missing
-#             df[!, key] = saved_values.saveval |> Map(_getproperty) |> collect
-#         end
-#         return prob, sol, df
-#     end
-# end
 """
 # Notes
 """
@@ -55,7 +5,6 @@ function sim(state0, __dyn, p=nothing;
         t0=0.0, tf=1.0, solver=nothing,  # DifferentialEquations.jl will find a default solver
         callback::DiffEqBase.DECallback=CallbackSet(),
         log_off=false,
-        # log_func=nothing,
         saveat=nothing,
         savestep=nothing,
         kwargs...,
@@ -71,17 +20,6 @@ function sim(state0, __dyn, p=nothing;
     end
     tspan = (t0, tf)
     prob = ODEProblem(__dyn, state0, tspan, p)
-    # function dyn__LOG__ end  # necessary
-    # if isinplace(prob)
-    #     @LOG function dyn(dx, x, p, t; kwargs...)
-    #         @nested_log :__NESTED_LOG__ __dyn(dx, x, p, t; kwargs...)
-    #     end  # @LOG generates logger function whose name is dyn__LOG__(x, t, integrator)
-    # else
-    #     # @LOG function dyn(x, p, t; kwargs...)
-    #     #     __dyn(x, p, t; kwargs...)
-    #     # end  # @LOG generates logger function whose name is dyn__LOG__(x, t, integrator)
-    #     error("Not yet tested")
-    # end
     log_func = nothing
     if isinplace(prob)
         log_func = (x, t, integrator::DiffEqBase.DEIntegrator; kwargs...) -> __dyn(zero.(x), x, integrator.p, t; kwargs...)
@@ -91,8 +29,6 @@ function sim(state0, __dyn, p=nothing;
     saved_values = nothing
     if log_off == false
         saved_values = SavedValues(Float64, Dict)
-        # cb_save = SavingCallback(datum_format, saved_values;
-        # cb_save = SavingCallback(dyn, saved_values;
         cb_save = SavingCallback(log_func, saved_values;
                                  saveat=saveat, tdir=Int(sign(tspan[2]-tspan[1])))
         callback = CallbackSet(callback, cb_save)  # save values "after all other callbacks"
@@ -104,8 +40,6 @@ function sim(state0, __dyn, p=nothing;
         df = DataFrame(time=saved_values.t)
         all_keys = (saved_values.saveval |> Map(keys) |> union)[1]
         for key in all_keys
-            # _getproperty(x) = isdefined(x, key) ? getproperty(x, key) : missing
-            # df[!, key] = saved_values.saveval |> Map(_getproperty) |> collect
             _getindex(x) = haskey(x, key) ? getindex(x, key) : missing
             df[!, key] = saved_values.saveval |> Map(_getindex) |> collect
         end
