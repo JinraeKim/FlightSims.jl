f3496c534dff8aebb48d3a8793b50132f69d4e5f
JinraeKim <kjl950403@gmail.com>
2021-06-29 15:20:48 +0900

    Add MultipleEnvs

---
+++ src/environments/basics/MultipleEnvs.jl
@@ -0,0 +1,24 @@
+struct MultipleEnvs{T <: AbstractEnv, S <: Union{Symbol, String}} <: AbstractEnv
+    envs_dict::Dict{S, T}
+end
+
+function State(envs::MultipleEnvs)
+    @unpack envs_dict = envs
+    function state(; state_pairs...)
+        nt = (; state_pairs...)
+        ComponentArray(nt)
+    end
+end
+
+function Dynamics!(envs::MultipleEnvs)
+    @unpack envs_dict = envs
+    @Loggable function dynamics!(dx, x, p, t; kwargs_pairs...)
+        for (key, kwargs) in kwargs_pairs
+            @nested_log key Dynamics!(envs_dict[key])(getproperty(dx, key),
+                                                      getproperty(x, key),
+                                                      p == nothing ? nothing : getproperty(p, key),
+                                                      t;
+                                                      kwargs...)
+        end
+    end
+end
