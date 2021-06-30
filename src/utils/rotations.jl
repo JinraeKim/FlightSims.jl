9a79b1389844c76241c4965f9621302f8f923928
JinraeKim <kjl950403@gmail.com>
2021-06-30 14:09:29 +0900

    Add docstring for `euler`

--- src/utils/rotations.jl
+++ src/utils/rotations.jl
@@ -1,3 +1,15 @@
-function euler(x::Rotations.RotXYZ)
-    [x.theta1, x.theta2, x.theta3]
+"""
+# Variables
+- ϕ: roll
+- θ: pitch
+- ψ: yaw
+# Notes
+- RotXYZ (Z first applied)
+- `R` denotes the rotation matrix from I (inertial) to B (body).
+"""
+function euler(R::Rotations.RotXYZ)
+    ϕ = R.theta1  # X
+    θ = R.theta2  # Y
+    ψ = R.theta3  # Z
+    [ϕ, θ, ψ]
 end
