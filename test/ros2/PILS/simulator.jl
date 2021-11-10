bdfbfec4af264f8d3891a53c60584b638e563d03
JinraeKim <kjl950403@gmail.com>
2021-11-10 13:28:36 +0900

    wip

--- test/ros2/PILS/simulator.jl
+++ test/ros2/PILS/simulator.jl
@@ -2,35 +2,35 @@ using PyCall
 
 rclpy = pyimport("rclpy")
 rosNode = pyimport("rclpy.node")
-FSim_msg = pyimport("fsim_interfaces.msg")
-np = pyimport("numpy")
+geoMsg = pyimport("geometry_msgs.msg")
 
 
 @pydef mutable struct StateNode <: rosNode.Node
-    function __init__(self, state0)
+    function __init__(self)
         rosNode.Node.__init__(self, "state_node")
         # publisher
-        self.publisher_ = self.create_publisher(FSim_msg.Data, "state", 10)
+        self.publisher_ = self.create_publisher(geoMsg.Point, "state", 10)
         timer_period = 0.5
         function timer_callback(self)
-            msg_state = FSim_msg.Data()
-            msg_state.data = self.state  # TODO: check it
-            # msg_state.data = self.state.tolist()
+            msg_state = geoMsg.Point()
+            msg_state.x = self.state[1]
+            msg_state.y = self.state[2]
+            msg_state.z = self.state[3]
             self.publisher_.publish(msg_state)
-            self.get_logger().info("state: $(msg_state.data)")
+            self.get_logger().info("state: $(self.state)")
             if self.control != nothing
                 self.state += -0.1*self.state
             end
         end
         self.timer = self.create_timer(timer_period, () -> timer_callback(self))
         # subscriber
-        self.state = state0
+        self.state = ones(3)
         self.control = zeros(3)  # TODO: change
         function listener_callback(self, msg_control)
             # self.control = np.array(msg_control.data)
             # self.get_logger().info("I heard: $(msg.data)")
         end
-        self.subscription = self.create_subscription(FSim_msg.Data, "control", msg -> listener_callback(self, msg), 10)
+        self.subscription = self.create_subscription(geoMsg.Point, "control", msg -> listener_callback(self, msg), 10)
     end
 end
 
