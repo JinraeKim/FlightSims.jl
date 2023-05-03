b0d40d3ad389da2303092fad852d4bf42aca5876
JinraeKim <kjl950403@gmail.com>
2023-05-04 00:10:25 +0900

    wip

--- test/environments/controllers/cbf.jl
+++ test/environments/controllers/cbf.jl
@@ -134,7 +134,7 @@ function position_cbf_full_dynamics(; Δt=0.005, Δt_save=0.05, tf=1.0)
     ol_controller = OuterLoopGeometricTrackingController()
     il_controller = InnerLoopGeometricTrackingController()
 
-    p0 = [-1.5, -1.0, -1.0]
+    p0 = [-1.0, -1.0, -1.0]
     X0_multicopter = State(multicopter)(p0)
     X0_il_controller = State(il_controller)()
     X0 = ComponentArray(
@@ -145,7 +145,7 @@ function position_cbf_full_dynamics(; Δt=0.005, Δt_save=0.05, tf=1.0)
     # p_d = t -> [0.4*sin(0.5*pi*t), 0.6*cos(0.5*pi*t), 0.4*t]
     # b_1_d = t -> [cos(0.1*pi*t), sin(0.1*pi*t), 0]
     p_d = function (t)
-        p = [0.2*t - 1.5, 0.02*t^2 + 0.05*t - 1.0, p0[3]]
+        p = [0.2*t - 1.0, 0.02*t^2 + 0.05*t - 1.0, p0[3]]
         return p
     end
     b_1_d = t -> [1, 0, 0]
@@ -158,13 +158,13 @@ function position_cbf_full_dynamics(; Δt=0.005, Δt_save=0.05, tf=1.0)
     allocator = PseudoInverseAllocator(multicopter.B)
     # CBF
     obstacles = [
-                 Circle(+0.4, +0.6, 0.30),
-                 Ellipse(-0.4, +0.7, 0.40, 0.30),
-                 Circle(-0.6, -0.5, 0.25),
+                 Circle(+0.9, +0.6, 0.30),
+                 Ellipse(+0.0, +0.7, 0.50, 0.30),
+                 Circle(-0.1, -0.5, 0.25),
                 ]
     hs = [generate_h(obs) for obs in obstacles]
-    α1 = x -> 2.5*x
-    α2 = x -> 2.5*x
+    α1 = x -> 2.0*x
+    α2 = x -> 10.0*x
     cbfs = [InputAffinePositionCBF((p, v) -> [0, 0, g], (p, v) -> -(1/m)*I(3), h, α1, α2) for h in hs]
 
     @Loggable function dynamics!(dX, X, params, t)
@@ -216,7 +216,7 @@ function position_cbf_full_dynamics(; Δt=0.005, Δt_save=0.05, tf=1.0)
     params0 = [0, 0, m*g]
     simulator = Simulator(X0, dynamics!, params0, tf=tf)
     cb = PeriodicCallback(affect!, Δt; initial_affect=true)
-    df = solve(simulator; callback=cb, savestep=Δt_save)
+    df = @time solve(simulator; callback=cb, savestep=Δt_save)
     ts = df.time
     ps = hcat([datum.multicopter.state.p for datum in df.sol]...)'
     p_xs = hcat([datum.multicopter.state.p[1] for datum in df.sol]...)'
@@ -226,39 +226,52 @@ function position_cbf_full_dynamics(; Δt=0.005, Δt_save=0.05, tf=1.0)
     p_refs = [p_d(t) for t in ts]
     p_ref_xs = [p[1] for p in p_refs]
     p_ref_ys = [p[2] for p in p_refs]
-    fig = plot(layout=(4, 1))
-    plot!(fig, ts, ps;
-          subplot=1,
-          label=["p_x" "p_y" "p_z"], lc=:blue, ls=[:solid :dash :dot],
-          legend=:outertopright,
-         )
-    plot!(fig, ts, hcat(p_refs...)';
-          subplot=1,
-          label=["r_x" "r_y" "r_z"], lc=:red, ls=[:solid :dash :dot],
-          legend=:outertopright,
-         )
-    plot!(fig, ts, u_saturateds;
-          subplot=2,
-          label=["u_1" "u_2" "u_3" "u_4"], lc=:blue, ls=[:solid :dash :dot :dashdot],
-          legend=:outertopright,
-         )
-    plot!(fig, ts, hcat([[1e-1, 1, 1, 1] .* ν for ν in νs]...)';
-          subplot=3,
-          label=["0.1 * f" "M_x" "M_y" "M_z"], lc=:blue, ls=[:solid :dash :dot :dashdot],
-          legend=:outertopright,
-         )
+    # fig = plot(layout=(4, 1))
+    # plot!(fig, ts, ps;
+    #       subplot=1,
+    #       label=["p_x" "p_y" "p_z"], lc=:blue, ls=[:solid :dash :dot],
+    #       legend=:outertopright,
+    #      )
+    # plot!(fig, ts, hcat(p_refs...)';
+    #       subplot=1,
+    #       label=["r_x" "r_y" "r_z"], lc=:red, ls=[:solid :dash :dot],
+    #       legend=:outertopright,
+    #      )
+    # plot!(fig, ts, u_saturateds;
+    #       subplot=2,
+    #       label=["u_1" "u_2" "u_3" "u_4"], lc=:blue, ls=[:solid :dash :dot :dashdot],
+    #       legend=:outertopright,
+    #      )
+    # plot!(fig, ts, hcat([[1e-1, 1, 1, 1] .* ν for ν in νs]...)';
+    #       subplot=3,
+    #       label=["0.1 * f" "M_x" "M_y" "M_z"], lc=:blue, ls=[:solid :dash :dot :dashdot],
+    #       legend=:outertopright,
+    #      )
+    # plot!(fig, p_xs, p_ys;
+    #       subplot=4,
+    #       lc=:blue,
+    #      )
+    # plot!(fig, p_ref_xs, p_ref_ys;
+    #       subplot=4,
+    #       lc=:red,
+    #       ls=:dash,
+    #      )
+    # for obs in obstacles
+    #     plot!(fig, shape(obs);
+    #           subplot=4,
+    #           seriestype=[:shape,], lw=0.5, c=:blue, linecolor=:black, legend=false, fillalpha=0.2, aspect_ratio=1,
+    #          )
+    # end
+    fig = plot()
     plot!(fig, p_xs, p_ys;
-          subplot=4,
           lc=:blue,
          )
     plot!(fig, p_ref_xs, p_ref_ys;
-          subplot=4,
           lc=:red,
           ls=:dash,
          )
     for obs in obstacles
         plot!(fig, shape(obs);
-              subplot=4,
               seriestype=[:shape,], lw=0.5, c=:blue, linecolor=:black, legend=false, fillalpha=0.2, aspect_ratio=1,
              )
     end
