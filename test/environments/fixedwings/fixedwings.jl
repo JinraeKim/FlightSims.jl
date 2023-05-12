cda040916ac4150b5579c44ae844919d2baddb61
Jinrae Kim <kjl950403@gmail.com>
2023-05-12 09:17:41 +0900

    Add wingrock and bump a new version (#159)
    * Add wingrock and bump a new version
    
    * Add fixedwings-wingrock
    
    * Bump a new version

---
+++ test/environments/fixedwings/fixedwings.jl
@@ -0,0 +1,1 @@
+include("wingrock.jl")
---
+++ test/environments/fixedwings/wingrock.jl
@@ -0,0 +1,43 @@
+using Test
+using FlightSims
+using ForwardDiff
+using Plots
+using ComponentArrays
+using LinearAlgebra
+
+
+function main(; Δt=0.01, tf=1.0)
+    env = WingRock()
+    x10, x20 = 0.3, 0.0
+    X0 = State(env)(x10, x20)
+
+    @Loggable function dynamics!(dX, X, p, t)
+        (; x1, x2) = X
+        @nested_log Dynamics!(env)(dX, X, p, t; u=-(x1+x2))
+    end
+    simulator = Simulator(X0, dynamics!, []; tf=tf)
+    df = solve(simulator; savestep=Δt)
+    ts = df.time
+    x1s = [datum.state.x1 for datum in df.sol]
+    x2s = [datum.state.x2 for datum in df.sol]
+    us = [datum.input for datum in df.sol]
+    fig = plot(layout=(3, 1))
+    plot!(fig, ts, x1s;
+          subplot=1,
+          label="x1",
+         )
+    plot!(fig, ts, x2s;
+          subplot=2,
+          label="x2",
+         )
+    plot!(fig, ts, us;
+          subplot=3,
+          label="u",
+         )
+    display(fig)
+end
+
+
+@testset "wingrock" begin
+    main()
+end
